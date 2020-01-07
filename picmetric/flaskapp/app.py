56861ae1f1db722307cf4c38f12038d4c00a0e3d
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2020-01-06 20:40:33 -0500

    adding template, blueprints, etc

--- picmetric/flaskapp/app.py
+++ picmetric/flaskapp/app.py
@@ -7,57 +7,25 @@ from log import startLog, LOGFILE
 
 from .models.persist import Persistent, download
 from .models import resnet, yolo
+from .routes.api import api
 
 def create_app(settings_override=None):
 	startLog(LOGFILE)
 	APP_LOG = logging.getLogger('root')
 
 	APP_LOG.info('Creating app...')
-    app = Flask(__name__, instance_relative_config=True)
+	app = Flask(__name__, instance_relative_config=True)
 
-    app.config.from_object('config.settings')
+	app.config.from_object('config.settings')
 
 	APP_LOG.info('Creating persistent object...')
 	persistent = Persistent()
 	APP_LOG.info('Flask instantiation complete.')
 
+	app.register_blueprint(api)
 
-	@app.route("/api", methods=['GET', 'POST'])
-	def api():
-		APP_LOG.info(f'''/api called, request values: {request.values}''')
-		url = request.values.get('url', None)
-		threshold = float(request.values.get('threshold', 0.2))
-
-		try:
-			img_path = download(url)
-		except Exception as e:
-			APP_LOG.warning(f'Failed to retrieve image at url: {url}')
-			APP_LOG.warning(e)
-			return jsonify({
-				'success': 'false',
-				'errortype': 'InvalidURL',
-				'parameters': {
-					'url': url,
-				},
-				'message': f'Unable to retrieve url: {url}',
-			})
-
-		APP_LOG.info('Image retrieved.')
-		APP_LOG.info('Running resnet50...')
-		resnet_results = resnet.predict(img_path, persistent, classification_threshold=threshold)
-		APP_LOG.info('Done resnet50.')
-		APP_LOG.info('Running YOLOv3...')
-		yolo_results = yolo.predict(img_path, persistent, classification_threshold=threshold)
-		APP_LOG.info('Done YOLOv3.')
-
-		results = {
-			'success': 'true',
-			'url': str(url),
-			'resnet50_objects': resnet_results,
-			'yolo_objects': yolo_results,
-		}
-		APP_LOG.info(f'Done predicting for image at url: {url}')
-
-		return(jsonify(results))
+    @app.route('/')
+    def redir():
+        return render_template('base.html')
 
 	return app
