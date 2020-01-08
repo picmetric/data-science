23407a02a99a5e1548fbd183539a458ecd504fdf
larkeith <llarkeith@gmail.com>
2020-01-08 14:01:27 -0800

    Move model loading and running to modelmanager Also, move modelmanager.py

--- picmetric/flaskapp/models/persist.py
+++ picmetric/flaskapp/models/persist.py
@@ -3,6 +3,7 @@
 import logging
 import requests
 import io
+import numpy
 
 from urllib.request import urlretrieve
 from . import resnet, yolo
@@ -10,6 +11,7 @@ from decouple import config
 from multiprocessing.managers import BaseManager
 
 
+class Non200ResponseError(Exception): pass
 # PERSIST_LOG = logging.getLogger('root')
 
 
@@ -26,6 +28,10 @@ def download(img_url):
 def retrieve_as_bytes(img_url):
 	print(f'Retrieving image at url: {img_url} ...')
 	with requests.get(img_url) as response:
+		if response.status_code != 200:
+			raise Non200ResponseError(
+				f'Received response with status code {response.status_code} while fetching url: {img_url}'
+			)
 		bytes_content = io.BytesIO(response.content)
 	# PERSIST_LOG.info(
 	print(f'''Downloaded image at url: {img_url}''')
@@ -35,8 +41,8 @@ class Persistent:
 	def __init__(self):
 		self.models = {}
 		self.modelmanager = BaseManager(('', config('MANAGER_PORT', cast=int)), bytes(config('MANAGER_AUTHKEY'), encoding='utf8'))
-		self.modelmanager.register('get_model')
-		self.modelmanager.register('set_model')
+		self.modelmanager.register('predict')
+		self.modelmanager.register('instantiate')
 		self.modelmanager.register('exists')
 		self.modelmanager.connect()
 		self.instantiate_models(self.modelmanager)
@@ -47,27 +53,28 @@ class Persistent:
 		# PERSIST_LOG.info(
 		print('Checking for resnet...')
 		# self.models['resnet']
-		if modelmanager.exists('resnet'):
+		if modelmanager.exists('resnet')._getvalue() is False:
 			print('Resnet not found, instantiating...')
-			modelmanager.set_model('resnet', resnet.instantiate_model())
+			modelmanager.instantiate('resnet')
+			print('Done loading resnet.')
+		else:
+			print('resnet already loaded.')
 		# resnet = modelmanager.get_model('resnet')
 		# PERSIST_LOG.info(
-		print('Done loading resnet.')
 
 		# PERSIST_LOG.info(
 		print('Checking for YOLOv3...')
-		if modelmanager.exists('yolo'):
+		if modelmanager.exists('yolo')._getvalue() is False:
 			print('yolo not found, instantiating...')
-			modelmanager.set_model('yolo', yolo.instantiate_model(yolo.YOLO_WEIGHTS_PATH))
+			modelmanager.instantiate('yolo')
+			print('Done loading YOLOv3.')
+		else:
+			print('yolo already loaded.')
 		# self.models['yolo'] = modelmanager.get_model('resnet', resnet.instantiate_model, yolo.YOLO_WEIGHTS_PATH)
 		# PERSIST_LOG.info(
-		print('Done loading YOLOv3.')
 
 	def predict_model(self, model, x):
-		if isinstance(model, str):
-			model = self.modelmanager.get_model(model)
+		predictions = self.modelmanager.predict(model, x)._getvalue()
 
-		predictions = model.predict(x)
-
-		return (predictions)
+		return predictions
 
---
+++ picmetric/modelmanager.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from multiprocessing import Lock
+from multiprocessing.managers import BaseManager
+from decouple import config
+from flaskapp.models import yolo, resnet
+
+model_modules = {
+	'yolo': yolo,
+	'resnet': resnet,
+}
+models = {}
+lock = Lock()
+
+
+def predict(model_name, x):
+	with lock:
+		result = models[model_name].predict(x)
+		return result
+
+
+def exists(model_name):
+	with lock:
+		return model_name in models
+
+
+def instantiate(model_name):
+	with lock:
+		model_module = model_modules[model_name]
+		model = model_module.instantiate_model()
+		models[model_name] = model
+
+
+def main():
+	manager = BaseManager(('', config('MANAGER_PORT', cast=int)), bytes(config('MANAGER_AUTHKEY'), encoding='utf8'))
+	manager.register('instantiate', instantiate)
+	manager.register('predict', predict)
+	manager.register('exists', exists)
+	server = manager.get_server()
+	server.serve_forever()
+
+
+if __name__ == '__main__':
+	main()
