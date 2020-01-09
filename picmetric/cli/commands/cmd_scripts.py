a3a6fac676049704d05e4b04b9f2dfaa9dc43c8d
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2020-01-09 08:22:54 -0500

    Update cmd_scripts.py

--- picmetric/cli/commands/cmd_scripts.py
+++ picmetric/cli/commands/cmd_scripts.py
@@ -3,6 +3,6 @@ import subprocess, click, os
 @click.command()
 @click.argument('path', default='picmetric')
 def cli(path):
-    full_path = os.path.join('scripts', path)
+    full_path = os.path.join('picmetric', 'scripts', path)
     cmd = f'python {full_path}'
     return subprocess.call(cmd, shell=True)
