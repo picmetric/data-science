5fb799dfc7013a6f05e4023019b48fd0d18d6dda
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2020-01-09 08:39:34 -0500

    testin workin dir

--- picmetric/cli/commands/cmd_scripts.py
+++ picmetric/cli/commands/cmd_scripts.py
@@ -3,6 +3,7 @@ import subprocess, click, os
 @click.command()
 @click.argument('path', default='picmetric')
 def cli(path):
-    full_path = os.path.join(os.path.dirname(__file__), 'picmetric', 'scripts', path)
-    cmd = f'python {full_path}'
+    # full_path = os.path.join(os.path.dirname(__file__), 'picmetric', 'scripts', path)
+    # cmd = f'python {full_path}'
+    cmd = 'python -c "print(os.getcwd())"'
     return subprocess.call(cmd, shell=True)
