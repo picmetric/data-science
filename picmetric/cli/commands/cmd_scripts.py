0f47f9de16e13e955ea05effdffca54e27eb9848
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2020-01-09 08:49:09 -0500

    Update cmd_scripts.py

--- picmetric/cli/commands/cmd_scripts.py
+++ picmetric/cli/commands/cmd_scripts.py
@@ -3,7 +3,15 @@ import subprocess, click, os
 @click.command()
 @click.argument('path', default='picmetric')
 def cli(path):
-    # full_path = os.path.join(os.path.dirname(__file__), 'picmetric', 'scripts', path)
-    # cmd = f'python {full_path}'
-    cmd = 'python -c "import os; print(os.getcwd())"'
+    # needs to be dependent on FLASK_ENV
+
+    # For runnin locally
+    # full_path = os.path.join('scripts', path)
+
+    # for docker
+    full_path = os.path.join('data-science', 'picmetric', 'scripts', path)
+    cmd = f'python {full_path}'
+
+    # for adjusting file path from inside docker
+    # cmd = 'python -c "import os; print(os.getcwd())"'
     return subprocess.call(cmd, shell=True)
